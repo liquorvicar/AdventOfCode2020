8949173f4eb2280fd848fca961547e16d9ee6998
Andy Clyde <me@andyclyde.com>
2020-12-28 16:10:24 +0000

    Day 20 [working, finally!]

--- src/Answer20.php
+++ src/Answer20.php
@@ -7,10 +7,40 @@ class Answer20 extends Base
 
     public function one(array $input)
     {
+        $grid = $this->findGrid($input);
+        $checksum = 1;
+        $firstRow = reset($grid[0]);
+        $checksum = $checksum * reset($firstRow) * end($firstRow);
+        $lastRow = end($grid[0]);
+        $checksum = $checksum * reset($lastRow) * end($lastRow);
+        return $checksum;
     }
 
     public function two(array $input)
     {
+        $grid = $this->findGrid($input);
+        $grid = $grid[1];
+        $grid = $this->stripBorders($grid);
+        $image = $this->formImage($grid);
+        $countMonsters = 0;
+        $countSquares = $this->countAllSquares($image);
+        $rotate = 0;
+        $flip = false;
+        while ($countMonsters === 0) {
+            $countMonsters = $this->countMonsters($image);
+            $image = $this->rotateTile($image);
+            $rotate++;
+            if ($rotate === 4) {
+                if ($flip) {
+                    break;
+                }
+                $image = $this->flipTile($image);
+                $flip = true;
+                $rotate = 0;
+            }
+        }
+        
+        return $countSquares - $countMonsters;
     }
 
     public function parseInput(array $input): array
@@ -44,11 +74,12 @@ class Answer20 extends Base
     {
         $tiles = $this->parseInput($input);
         $matchingGrid = [];
+        $square = sqrt(count($tiles));
 
         foreach ($tiles as $tileId => $tile) {
             $remainingTiles = $tiles;
             unset($remainingTiles[$tileId]);
-            $matchingGrid = $this->findMatchingGrid($tile, [[$tileId]], [], $remainingTiles);
+            $matchingGrid = $this->findMatchingGrid($tile, [[$tileId]], [], $remainingTiles, $square);
             if (!empty($matchingGrid)) {
                 break;
             }
@@ -57,7 +88,7 @@ class Answer20 extends Base
         return $matchingGrid;
     }
 
-    private function findMatchingGrid($tile, array $gridIds, array $grid, array $remainingTiles)
+    private function findMatchingGrid($tile, array $gridIds, array $grid, array $remainingTiles, int $squareSize)
     {
         $currentRow = max(array_keys($gridIds));
         $row = $gridIds[$currentRow];
@@ -78,7 +109,7 @@ class Answer20 extends Base
                     $grid[$currentRow][$currentCol] = $tile;
                     $newCol = $currentCol + 1;
                     $newRow = $currentRow;
-                    if ($newCol > 2) {
+                    if ($newCol > ($squareSize - 1)) {
                         $newCol = 0;
                         $newRow = $currentRow + 1;
                     }
@@ -91,9 +122,11 @@ class Answer20 extends Base
                         $newGridIds[$newRow][$newCol] = $remainingTileId;
                         $newRemainingTiles = $remainingTiles;
                         unset($newRemainingTiles[$remainingTileId]);
-                        $newGrid = $this->findMatchingGrid($remainingTile, $newGridIds, $grid, $newRemainingTiles);
-                        if (!empty($newGrid)) {
-                            if (count($newGrid) === 3 && count($newGrid[2]) === 3) {
+                        $foundGrid = $this->findMatchingGrid($remainingTile, $newGridIds, $grid, $newRemainingTiles, $squareSize);
+                        if (!empty($foundGrid)) {
+                            $newGrid = $foundGrid[0];
+                            if (count($newGrid) === $squareSize && count(end($newGrid)) === $squareSize) {
+                                $grid = $foundGrid[1];
                                 $gridIds = $newGrid;
                                 $foundMatch = true;
                                 break 3;
@@ -107,7 +140,7 @@ class Answer20 extends Base
             $tile = $this->flipTile($tile);
         } while ($flipped === true);
 
-        return $foundMatch ? $gridIds : [];
+        return $foundMatch ? [$gridIds, $grid] : [];
     }
 
     private function matchesTop($tile, $tileAbove): bool
@@ -150,6 +183,84 @@ class Answer20 extends Base
         }
         ksort($newTile);
         return $newTile;
+    }
+
+    private function stripBorders($grid)
+    {
+        return array_map(function ($row) {
+            return array_map(function ($tile) {
+                $newTile = [];
+                array_shift($tile);
+                array_pop($tile);
+                while (!empty($tile)) {
+                    $row = array_shift($tile);
+                    array_shift($row);
+                    array_pop($row);
+                    $newTile[] = $row;
+                }
+                return $newTile;
+            }, $row);
+        }, $grid);
+    }
+
+    private function formImage(array $grid)
+    {
+        return array_reduce($grid, function ($newGrid, $row) {
+            foreach ($row[0] as $tileRowNum => $tileRow) {
+                $newTileRow = array_reduce($row, function ($newTileRow, $tile) use ($tileRowNum) {
+                    return array_merge($newTileRow, $tile[$tileRowNum]);
+                }, []);
+                $newGrid[] = $newTileRow;
+            }
+            return $newGrid;
+        }, []);
+    }
+
+    private function countAllSquares(array $image)
+    {
+        return array_reduce($image, function ($count, $row) {
+            $squares = array_filter($row, function ($bit) {
+                return $bit === '#';
+            });
+            return $count + count($squares);
+        }, 0);
+    }
+
+    public function countMonsters(array $image)
+    {
+        $seaMonster = [
+            [0, 18],
+            [1, 0],
+            [1, 5],
+            [1, 6],
+            [1, 11],
+            [1, 12],
+            [1, 17],
+            [1, 18],
+            [1, 19],
+            [2, 1],
+            [2, 4],
+            [2, 7],
+            [2, 10],
+            [2, 13],
+            [2, 16],
+        ];
+        $count = 0;
+        foreach ($image as $y => $row) {
+            foreach ($row as $x => $dot) {
+                $found = 1;
+                foreach ($seaMonster as $vector) {
+                    list($vectorY, $vectorX) = $vector;
+                    $target = $image[$vectorY + $y][$vectorX + $x] ?? '.';
+                    if ($target !== '#') {
+                        $found = 0;
+                        break;
+                    }
+                }
+                $count+= $found;
+            }
+        }
+        return $count * count($seaMonster);
     }
 }
 
